f4167eeb59563ba38080886623c061398f54de51
mposs93 <54368675+mposs93@users.noreply.github.com>
2019-09-02 17:13:36 -0500

    Made a wider variety of tests
    added NotNull and Null tests to MulitplicationTest

--- src/MulitplicationTest.java
+++ src/MulitplicationTest.java
@@ -8,13 +8,28 @@ import RandomMaths.Multiplication;
 public class MulitplicationTest {
 	int solution;
 	int baseNumber = 25;
-	
+	boolean bigIfTrue = false;
 	Multiplication multi = new Multiplication(baseNumber);
 	
 	@Before
 	public void setUp() throws Exception {
+		if (solution == 0) {
+			bigIfTrue = true;
+		}
 	}
 
+	//ensuring that the value isn't null
+	@Test
+	public void isNotNullTest() {
+		assertNotNull(multi.exponentialGrowth());
+	}
+	
+	//verifying that the variable solution is actually zero before inputting values into it
+	@Test
+	public void solutionIsZero() {
+		assertTrue(bigIfTrue);
+	}
+	
 	@Test
 	public void multipliTest() {
 		solution = baseNumber * baseNumber;
